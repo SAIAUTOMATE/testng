b18c494d0b7cb4a7658ad8cf0985620a2d4b9ed9
vogetis <saikrishna.vogeti@cdk.com>
2024-07-16 10:47:09 +0530

    tesgng named as cuucmber

---
+++ AutomationCucucmber/src/test/java/TestNG/TestSelenium.java
@@ -0,0 +1,21 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class TestSelenium {
+@Test
+void stringReverse()
+
+{
+	
+	int a=10;
+	int b=20;
+	
+	a=a+b;
+	a=a-b;
+	a=a-b;
+
+System.out.println("rest");
+System.out.println("swapped:: a="+"     "+a+"b="+b);
+}
+}
